8bb15df6f2281f3bce8965a49becfb76a72c9869
AaryaR <77625871+aaaaryaaa@users.noreply.github.com>
2024-01-03 21:53:25 +0530

    Delete frontend directory

--- frontend/src/pages/About.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function About() {
-  return (
-    <div><h2>THIS IS ABOUT PAGE</h2></div>
-  )
-}
--- frontend/src/pages/Home.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Home() {
-  return (
-    <div><h2>THIS IS HOME PAGE</h2></div>
-  )
-}
